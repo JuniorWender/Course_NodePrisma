1713e7171872146b341b789869a5ed18e398b4b8
wender.junior <wender.junior@etaure.com.br>
2023-05-15 09:56:18 -0300

    Start Project And Create 2 Migrations

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,13 @@
+import { CreateUserDTO } from "../../dtos/CreateUserDTO";
+
+export class CreateUserUseCase {
+
+    async execute({name, email} : CreateUserDTO) {
+        // Verify if user alredy exists
+
+        
+        
+        // Logic to create a new user
+
+    }
+}
